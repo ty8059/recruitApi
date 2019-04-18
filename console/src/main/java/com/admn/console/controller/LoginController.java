0365a714030d618b386eb0453b8e42032c09030b
wangyi <759046694@qq.com>
2019-04-19 00:22:52 +0800

    登录验证完成

--- console/src/main/java/com/admn/console/controller/LoginController.java
+++ console/src/main/java/com/admn/console/controller/LoginController.java
@@ -1,7 +1,10 @@
 package com.admn.console.controller;
 
 import com.admn.common.ResultEntity;
+import com.admn.console.model.TblUser;
 import com.admn.console.service.LoginService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,9 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 @RequestMapping("login")
 public class LoginController {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
 
     @Autowired
     private LoginService loginService;
@@ -23,13 +30,17 @@ public class LoginController {
     }
 
     @PostMapping("doLogin")
-    public ResultEntity doLogin(String username, String password) {
+    public ResultEntity doLogin(String username, String password, HttpSession session) {
         if (username == null || "".equals(username)) {
             return new ResultEntity(false, "请输入用户名");
         }
         if (password == null || "".equals(password)) {
             return new ResultEntity(true, "请输入密码");
         }
-        return loginService.login(username, password);
+        ResultEntity resultEntity = loginService.login(username, password);
+        if (resultEntity.isSuccess()) {
+            session.setAttribute("sysUser", (TblUser) resultEntity.getObj());
+        }
+        return resultEntity;
     }
 }
