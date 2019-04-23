1e74dcdee8386d8f265b27a8c770ba73383d038e
wangyi <759046694@qq.com>
2019-04-24 00:29:25 +0800

    职位管理转义完成

--- console/src/main/java/com/admn/console/dao/TblPositionAppMapper.java
+++ console/src/main/java/com/admn/console/dao/TblPositionAppMapper.java
@@ -2,6 +2,7 @@ package com.admn.console.dao;
 
 import com.admn.console.model.TblPositionApp;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
@@ -10,6 +11,9 @@ public interface TblPositionAppMapper extends Mapper<TblPositionApp> {
     int insert(TblPositionApp record);
 
     int insertSelective(TblPositionApp record);
+
+    @Select("select t1.* from TBL_POSITION_APP t1 where t1.USER_ID = #{userId} and t1.POSITION_ID = #{positionId} and t1.RESUME_ID = #{resumeId}")
+    TblPositionApp findAppByUserId(@Param("userId") Integer userId, Integer positionId, Integer resumeId);
 
     List<TblPositionApp> findAppByAppTimeAndStatus(@Param("startTime") String startTime,
                                                    @Param("endTime") String endTime,
--- console/src/main/java/com/admn/console/service/PositionAppService.java
+++ console/src/main/java/com/admn/console/service/PositionAppService.java
@@ -3,9 +3,12 @@ package com.admn.console.service;
 import com.admn.common.Layui;
 import com.admn.common.Page;
 import com.admn.common.ResultEntity;
+import com.admn.console.model.TblPositionApp;
 
 public interface PositionAppService {
+    TblPositionApp getAppByUserIdAndPositionIdAndResumeId(Integer userId, Integer positionId, Integer resumeId);
+
     Layui getAppByAppTimeAndStatus(String startTime, String endTime, String status, Page page);
 
-    ResultEntity processApp(Integer userId, Integer positionId, Integer resumeId, String status);
+    ResultEntity processApp(TblPositionApp positionApp);
 }
